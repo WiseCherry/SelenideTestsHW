fea80d74a84c013a92d799c15e9ffc19dc092efb
VadimYurtaev <vadikyu@gmail.com>
2025-10-15 22:06:40 +0300

    added allure

--- src/test/java/tests/APItests/LoginExtendedTests.java
+++ src/test/java/tests/APItests/LoginExtendedTests.java
@@ -1,10 +1,14 @@
 package tests.APItests;
 
-import models.LoginBodyModel;
-import models.LoginResponseModel;
+import io.qameta.allure.restassured.AllureRestAssured;
+import models.lombok.LoginBodyLombokModel;
+import models.lombok.LoginResponseLombokModel;
+import models.pojo.LoginBodyModel;
+import models.pojo.LoginResponseModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
@@ -34,9 +38,9 @@ public class LoginExtendedTests {
 
     }
 
-    @DisplayName("Тест логина - как надо делать")
+    @DisplayName("Тест логина - pojo")
     @Test
-    void successfulLoginTest() {
+    void successfulLoginPojoTest() {
 
         LoginBodyModel authData = new LoginBodyModel();
         authData.setEmail("test@test.ru");
@@ -56,7 +60,100 @@ public class LoginExtendedTests {
                 .statusCode(200)
                 .extract().as(LoginResponseModel.class);
 
-                assertEquals("11111", responce.getToken());
+        assertEquals("11111", responce.getToken());
 
+    }
+
+    @DisplayName("Тест логина - lombok")
+    @Test
+    void successfulLoginLombokTest() {
+
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("test@test.ru");
+        authData.setPassword("123123");
+
+        LoginResponseLombokModel response = given()
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .body(authData)
+                .contentType(JSON)
+
+                .when()
+                .get("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("error", response.getToken());
+    }
+
+    @DisplayName("Тест логина - allure")
+    @Test
+    void successfulLoginAllureTest() {
+
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("test@test.ru");
+        authData.setPassword("123123");
+
+        LoginResponseLombokModel loginResponseLombokModel = new LoginResponseLombokModel();
+        loginResponseLombokModel.setError("Missing API key");
+
+        LoginResponseLombokModel response = given()
+
+                .filter(new AllureRestAssured())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .body(authData)
+                .contentType(JSON)
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(401)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("Missing API key", response.getError());
+    }
+
+    @DisplayName("Тест логина - allure")
+    @Test
+    void successfulLoginWithStepsTest() {
+
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("test@test.ru");
+        authData.setPassword("123123");
+
+        LoginResponseLombokModel loginResponseLombokModel = new LoginResponseLombokModel();
+        loginResponseLombokModel.setError("Missing API key");
+
+        LoginResponseLombokModel response = step("Make request", () ->
+                 given()
+
+                    .filter(new AllureRestAssured())
+                    .log().uri()
+                    .log().body()
+                    .log().headers()
+                    .body(authData)
+                    .contentType(JSON)
+
+                 .when()
+                    .post("https://reqres.in/api/login")
+
+                 .then()
+                    .log().status()
+                    .log().body()
+                    .statusCode(401)
+                    .extract().as(LoginResponseLombokModel.class));
+
+        step("Check response", () ->
+            assertEquals("Missing API key", response.getError()));
     }
 }
